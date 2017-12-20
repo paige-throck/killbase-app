ddc5e890fe383f8aff6982494d7c8592512a3070
Paige Throckmorton <path4942@gmail.com>
2017-12-20 10:50:56 -0600

    Added env

--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,24 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
-  }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: 'ec2-184-72-228-128.compute-1.amazonaws.com',
+      database:'d36j6suc27t9pa',
+      user: 'vibrabcqibkfey',
+      port: '5432',
+      password: '33e8e63bf5bed22ea2b74318388ac75d78fa1bd5dd20c4ba87c81b616a4b6e38'
+
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
 
 };
